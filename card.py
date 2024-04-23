ce74b0e3717bc8ee7d766cda4c1f6ab5a5c34ecd
nnandani25 <navya.nandani@menloschool.org>
2024-04-23 09:53:40 -0700

    get_clue works, board prints

--- card.py
+++ card.py
@@ -2,6 +2,5 @@ class Card:
     def __init__(self, word):
         self.type = None
         self.word = word
-        self.in_game = True
         self.is_guessed = False
     
--- game.py
+++ game.py
@@ -7,7 +7,7 @@ class Game:
         # Load the words from file
         self.words = self.load_words(filename)
 
-        self.synonyms = self.get_synonyms()
+        # self.synonyms = self.get_synonyms()
 
         self.cards = [Card(word) for word in random.sample(self.words,25)]
         
@@ -34,7 +34,7 @@ class Game:
             words = [line.lower().strip() for line in file.readlines()]
         return words
     
-    def get_synonyms(self):
+    def get_synonyms(self, word):
         """
         Generates all synonyms for self.word
         based off of WordNet
@@ -47,29 +47,62 @@ class Game:
         # TODO: Complete this function
 
         # Loop through all the synsets for the word
-        for syn in wn.synsets(self.word):
+        for syn in wn.synsets(word):
             # Loop through all lemmas in the synset
             for lemma in syn.lemmas():
                 if lemma.name() not in synonyms:
-                    synonyms.append(lemma.name())
-        synonyms.remove(self.word)
-        return synonyms
+                    synonyms.append(lemma.name().lower())
+        synonyms.remove(word)
+        print("word:", word)
 
-    def give_clue(self):
-        
-        hint_word = random.choice(self.synonyms)
-        while hint_word == self.word or hint_word in self.word:
-            hint_word = random.choice(self.synonyms)
+        most_sim = synonyms[0]
+        highest_sim = -100
+        for i in synonyms:
+            if self.calculate_similarity(i, word) > highest_sim:
+                highest_sim = self.calculate_similarity(i, word)
+                most_sim = i
+        # print("Most_sim", most_sim)
+        return most_sim
 
-            w1 = wn.synsets(self.word)[0]
-            w2 = wn.synsets(hint_word)[0]
-            score = w1.wup_similarity(w2)
-            print(str(hint_word) + " has a score of " + str(score))
-            self.synonyms.remove(hint_word)
+    def give_clue(self, player):
+        # if player == self.player1:
+        #     available_cards = []
+        #     for card in self.p1_words:
+        #         if card.is_guessed == False:
+        #             available_cards.append(card)
+
+        #     hint_word = self.get_synonyms(random.choice(available_cards).word)
+        #     print("HINT", hint_word)
+            # return "HINT" + hint_word
+            
+        available_cards = []
+        if player == self.player1:
+            for card in self.p1_words:
+                if card.is_guessed == False:
+                    available_cards.append(card)
         
-        return hint_word
+        else:
+            for card in self.p2_words:
+                if card.is_guessed == False:
+                    available_cards.append(card)
+
+        hint_word = self.get_synonyms(random.choice(available_cards).word)
+        print("HINT", hint_word)
+
+
+        # hint_word = random.choice(self.synonyms)
+        # while hint_word == self.word or hint_word in self.word:
+        #     hint_word = random.choice(self.synonyms)
+
+        #     w1 = wn.synsets(self.word)[0]
+        #     w2 = wn.synsets(hint_word)[0]
+        #     score = w1.wup_similarity(w2)
+        #     print(str(hint_word) + " has a score of " + str(score))
+        #     self.synonyms.remove(hint_word)
+        
+        # return hint_word
     
-    def calculate_similarity(self, guess, mode=0):
+    def calculate_similarity(self, guess, card, mode=0):
         """
         Calculates and returns the similarity of 
         guess and self.word.
@@ -83,7 +116,7 @@ class Game:
             float: similarity score
         """
         # TODO: Complete this function
-        w1 = wn.synsets(self.word)[0]
+        w1 = wn.synsets(card)[0]
         w2 = wn.synsets(guess)[0]
         score = w1.wup_similarity(w2)
 
@@ -100,20 +133,41 @@ class Game:
 
         # Instructions
         print("\nWelcome to Contexto!")
-        print("Blank:", self.copy_cards)
-        print("Player 1", self.p1_words)
-        print("Player 2", self.p2_words)
-        print("Death", self.assasin_card)
+        print("Blank:", end =" ")
+        for blank in self.blank_cards:
+            print(blank.word, end=" ")
 
-        turn = self.player1
+        print("\nPlayer 1:", end=" ")
+        for p1c in self.p1_words:
+            print(p1c.word, end=" ")
+
+        print("\nPlayer 2:", end=" ")
+        for p2c in self.p2_words:
+            print(p2c.word, end = " ")
+
+        print("\nDeath: ", self.assasin_card.word)
+        print("")
+
+        current_player = self.player1
+        random.shuffle(self.cards)
+        
         # # Main game loop
         while True:
-            print(self.cards)
-            self.give_clue()
-            guess = input("Guess: ")
+            # print(self.cards)
+            count = 0
+            for i in self.cards:
+                print(i.word, end="\t\t")
+                count+=1
+                if count%5 == 0:
+                    print("\n")
+            
 
+            self.give_clue(current_player)
+            guess = input("Guess: ")
+            
             for card in self.cards:
-                if guess == card:
+                if guess == card.word:
+                    # print("YAY")
                     if card.is_guessed == True:
                         print("NO")
                         break
@@ -122,36 +176,38 @@ class Game:
                         print("DEAD")
                         break
                     
-                    elif card in self.p1_words and turn == self.player1:
+                    elif card in self.p1_words and current_player == self.player1:
                         print("YAY")
                         card.is_guessed = True
+                        card.word = card.word.upper()
                         self.player1.score += 1
-                        turn = self.player2
+                        current_player = self.player2
                         break
                     
-                    elif card in self.p2_words and turn == self.player2:
+                    elif card in self.p2_words and current_player == self.player2:
                         print("YAY")
                         card.is_guessed = True
+                        card.word = card.word.upper()
                         self.player2.score += 1
-                        turn = self.player1
+                        current_player = self.player1
                         break
             
 
 
-            # TODO: Complete the options
-            if guess == "h":
-                # self.get_a_hint()
+            # # TODO: Complete the options
+            # if guess == "h":
+            #     # self.get_a_hint()
 
-            elif guess == "q":
-                print(self.word)
-                break
+            # elif guess == "q":
+            #     print(self.word)
+            #     break
             
-            elif guess == self.word:
-                print("Yay you win!!!")
-                break
+            # elif guess == self.word:
+            #     print("Yay you win!!!")
+            #     break
             
-            else:
-                print(self.calculate_similarity(guess))
+            # else:
+            #     print(self.calculate_similarity(guess))
                 # guess = input("Guess a word, enter 'h' for a hint, or enter 'q' to reveal the answer: ")
 
 
